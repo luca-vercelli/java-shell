d51e2117a192adda213f980dc18a994f62005709
LLuca Vercelli <luca.vercelli.to@gmail.com>
2016-01-21 13:46:47 +0100

    Better tests

--- src/test/java/javax/shell/test/TestProcess.java
+++ src/test/java/javax/shell/test/TestProcess.java
@@ -66,182 +66,112 @@ public class TestProcess {
 
 	@Test
 	public void testPipelines() throws IOException {
-		final List<Integer> processes = new ArrayList<Integer>();
-		final List<String> lines = new ArrayList<String>();
+		final List<Integer> processesRan = new ArrayList<Integer>();
+		final List<String> linesReceived = new ArrayList<String>();
 
 		Process p1 = new Process() {
 			@Override
 			public void runme() throws Exception {
-				processes.add(1);
+				processesRan.add(1);
 				stdout.println("ehlo");
 				stdout.println("mydarling");
 				stdout.println("seeyou");
 			}
 		};
 
-		Process p2 = new Process() {
-			@Override
-			public void runme() throws Exception {
-				processes.add(2);
-				while (stdinReader.ready()) {
-					String l = stdinReader.readLine();
-					lines.add(l);
-				}
-			}
-		};
+		Process p2 = new TesterProcess(2, processesRan, linesReceived);
 
-		p1.pipe(p2).start();
+		p1.pipe(p2).sh();
 
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-		}
-
-		assertEquals("Two processes ran", 2, processes.size());
-		assertEquals("3 lines should be elaborated", 3, lines.size());
+		assertEquals("Two processes ran", 2, processesRan.size());
+		assertEquals("3 lines should be elaborated", 3, linesReceived.size());
 	}
 
 	@Test
 	public void testAnd() {
-		final List<Integer> processes = new ArrayList<Integer>();
-		final List<String> lines = new ArrayList<String>();
+		final List<Integer> processesRan = new ArrayList<Integer>();
+		final List<String> linesReceived = new ArrayList<String>();
 
 		Process p1 = new Process() {
 			@Override
 			public void runme() throws Exception {
-				processes.add(1);
+				processesRan.add(1);
 				stdout.println("ehlo");
 				stdout.println("mydarling");
 				stdout.println("seeyou");
 			}
 		};
 
-		Process p2 = new Process() {
-			@Override
-			public void runme() throws Exception {
-				processes.add(2);
-				while (stdinReader.ready()) {
-					String l = stdinReader.readLine();
-					lines.add(l);
-				}
-			}
-		};
+		Process p2 = new TesterProcess(2, processesRan, linesReceived);
 
-		p1.and(p2).start();
+		p1.and(p2).sh();
 
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-		}
-
-		assertEquals("Two processes ran", 2, processes.size());
-		assertEquals("0 lines should be elaborated", 0, lines.size());
+		assertEquals("Two processes ran", 2, processesRan.size());
+		assertEquals("0 lines should be elaborated", 0, linesReceived.size());
 	}
 
 	@Test
 	public void testAnd2() {
-		final List<Integer> processes = new ArrayList<Integer>();
-		final List<String> lines = new ArrayList<String>();
+		final List<Integer> processesRan = new ArrayList<Integer>();
+		final List<String> linesReceived = new ArrayList<String>();
 
 		Process p1 = new Process() {
 			@Override
 			public void runme() throws Exception {
-				processes.add(1);
+				processesRan.add(1);
 				throw new IllegalStateException();
 			}
 		};
 
-		Process p2 = new Process() {
-			@Override
-			public void runme() throws Exception {
-				processes.add(2);
-				while (stdinReader.ready()) {
-					String l = stdinReader.readLine();
-					lines.add(l);
-				}
-			}
-		};
+		Process p2 = new TesterProcess(2, processesRan, linesReceived);
 
-		p1.and(p2).start();
+		p1.and(p2).sh();
 
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-		}
-
-		assertEquals("1 processes ran", 1, processes.size());
-		assertEquals("0 lines should be elaborated", 0, lines.size());
+		assertEquals("1 processes ran", 1, processesRan.size());
+		assertEquals("0 lines should be elaborated", 0, linesReceived.size());
 	}
 
 	@Test
 	public void testOr() {
-		final List<Integer> processes = new ArrayList<Integer>();
-		final List<String> lines = new ArrayList<String>();
+		final List<Integer> processesRan = new ArrayList<Integer>();
+		final List<String> linesReceived = new ArrayList<String>();
 
 		Process p1 = new Process() {
 			@Override
 			public void runme() throws Exception {
-				processes.add(1);
+				processesRan.add(1);
 				stdout.println("ehlo");
 				stdout.println("mydarling");
 				stdout.println("seeyou");
 			}
 		};
 
-		Process p2 = new Process() {
-			@Override
-			public void runme() throws Exception {
-				processes.add(2);
-				while (stdinReader.ready()) {
-					String l = stdinReader.readLine();
-					lines.add(l);
-				}
-			}
-		};
+		Process p2 = new TesterProcess(2, processesRan, linesReceived);
 
-		p1.or(p2).start();
+		p1.or(p2).sh();
 
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-		}
-
-		assertEquals("1 process ran", 1, processes.size());
-		assertEquals("0 lines should be elaborated", 0, lines.size());
+		assertEquals("1 process ran", 1, processesRan.size());
+		assertEquals("0 lines should be elaborated", 0, linesReceived.size());
 	}
 
 	@Test
 	public void testOr2() {
-		final List<Integer> processes = new ArrayList<Integer>();
-		final List<String> lines = new ArrayList<String>();
+		final List<Integer> processesRan = new ArrayList<Integer>();
+		final List<String> linesReceived = new ArrayList<String>();
 
 		Process p1 = new Process() {
 			@Override
 			public void runme() throws Exception {
-				processes.add(1);
+				processesRan.add(1);
 				throw new IllegalStateException();
 			}
 		};
 
-		Process p2 = new Process() {
-			@Override
-			public void runme() throws Exception {
-				processes.add(2);
-				while (stdinReader.ready()) {
-					String l = stdinReader.readLine();
-					lines.add(l);
-				}
-			}
-		};
+		Process p2 = new TesterProcess(2, processesRan, linesReceived);
 
-		p1.or(p2).start();
+		p1.or(p2).sh();
 
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-		}
-
-		assertEquals("2 processes ran", 2, processes.size());
-		assertEquals("0 lines should be elaborated", 0, lines.size());
+		assertEquals("2 processes ran", 2, processesRan.size());
+		assertEquals("0 lines should be elaborated", 0, linesReceived.size());
 	}
 }
