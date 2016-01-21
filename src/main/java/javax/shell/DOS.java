7a7b49d4c2839d05105d103b6d2cc217c88f9b74
luca-vercelli <luca.vercelli.to@gmail.com>
2016-01-21 21:24:24 +0100

    DOS day 2

--- src/main/java/javax/shell/DOS.java
+++ src/main/java/javax/shell/DOS.java
@@ -1,14 +1,31 @@
 package javax.shell;
 
+/**
+ * DOS prompt commands. TODO.
+ * 
+ * @author luca vercelli 2016
+ */
 public class DOS {
-	
-	// TODO
-	
+
 	/**
 	 * DOS program <code>dir</code> (i.e. list directory).
 	 */
 	public static Process dir(String... args) {
 		return Unix.ls(args);
+	}
+
+	/**
+	 * DOS program <code>echo</code> (i.e. print to stdout).
+	 */
+	public static Process echo(String... args) {
+		return Unix.echo(args);
+	}
+
+	/**
+	 * DOS program <code>find</code> (i.e. find text inside files).
+	 */
+	public static Process find(String text, String... files) {
+		return Unix.grep(text, files);
 	}
 
 }
