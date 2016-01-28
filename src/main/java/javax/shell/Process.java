b5dba43ac79eecac86c41eae566b58f00844edf1
LLuca Vercelli <luca.vercelli.to@gmail.com>
2016-01-28 13:06:35 +0100

    moved code to Shell

--- src/main/java/javax/shell/Process.java
+++ src/main/java/javax/shell/Process.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,11 +15,7 @@ import java.io.PipedOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.Stack;
-import java.util.regex.Pattern;
 
 /**
  * Main class for *NIX based programs.
@@ -73,8 +68,7 @@ public abstract class Process extends Thread {
 	 * Standard input, supporting readLine(). This must <b>always</b> point to
 	 * stream stdin.
 	 */
-	protected BufferedReader stdinReader = new BufferedReader(
-			new InputStreamReader(System.in));
+	protected BufferedReader stdinReader = new BufferedReader(new InputStreamReader(System.in));
 
 	/**
 	 * Standard output
@@ -150,7 +144,7 @@ public abstract class Process extends Thread {
 
 	public List<String> expArgs() {
 		if (expArgs == null) {
-			expArgs = expand(args);
+			expArgs = shell.expand(args);
 		}
 		return expArgs;
 	}
@@ -186,8 +180,7 @@ public abstract class Process extends Thread {
 		try {
 			runme();
 		} catch (Exception e) {
-			System.err.println("Unhandled exception in Thread "
-					+ this.getName());
+			System.err.println("Unhandled exception in Thread " + this.getName());
 			e.printStackTrace(System.err);
 		}
 		if ((stdout != null) && (stdout != System.out))
@@ -390,8 +383,7 @@ public abstract class Process extends Thread {
 	/**
 	 * Create a list of FileInputStream, if any, or stdin.
 	 */
-	public List<InputStream> getInputStreams(List<String> files)
-			throws FileNotFoundException {
+	public List<InputStream> getInputStreams(List<String> files) throws FileNotFoundException {
 		List<InputStream> ret = new ArrayList<InputStream>();
 		if (files.isEmpty())
 			ret.add(stdin);
@@ -405,8 +397,7 @@ public abstract class Process extends Thread {
 	/**
 	 * Create a list of BufferedReader, if any, or stdin.
 	 */
-	public List<BufferedReader> getReaders(List<String> files)
-			throws FileNotFoundException {
+	public List<BufferedReader> getReaders(List<String> files) throws FileNotFoundException {
 		List<BufferedReader> ret = new ArrayList<BufferedReader>();
 		if (files.isEmpty())
 			ret.add(stdinReader);
@@ -420,8 +411,7 @@ public abstract class Process extends Thread {
 	/**
 	 * Create a list of FileOutputStream, if any, or stdout.
 	 */
-	public List<PrintStream> getOutputStreams(List<String> files, boolean append)
-			throws FileNotFoundException {
+	public List<PrintStream> getOutputStreams(List<String> files, boolean append) throws FileNotFoundException {
 		List<PrintStream> ret = new ArrayList<PrintStream>();
 		if (files.isEmpty())
 			ret.add(stdout);
@@ -433,161 +423,12 @@ public abstract class Process extends Thread {
 	}
 
 	/**
-	 * Return true if path is absolute (i.e. /some/p*ath/ or C:\so?me\path ).
-	 */
-	protected static boolean isAbsolute(String path) {
-
-		if (path == null)
-			throw new IllegalArgumentException("null path given");
-		path = path.trim();
-		if (path.equals(""))
-			throw new IllegalArgumentException("empty path given");
-
-		if (File.separator.equals("/")) {
-			// *NIX
-			return path.length() >= 1 && path.charAt(0) == '/';
-
-		} else if (File.separator.equals("\\")) {
-			// Windows
-			return path.length() >= 2 && path.charAt(1) == ':';
-
-		} else {
-			throw new IllegalStateException(
-					"Unsupported operating system! Please report this.");
-		}
-	}
-
-	/**
-	 * Split the path in pieces according to File.separator. The first piece is
-	 * the "root": it is the path's root ( "/" or "C:\" ) if the given path is
-	 * absolute, or current folder if it is relative.
-	 */
-	protected String[] splitRoot(String path) {
-
-		String[] pieces = path.split(File.separator);
-
-		if (!isAbsolute(path)) {
-
-			String[] result = new String[pieces.length + 1];
-			result[0] = getCurrentFolder();
-			for (int i = 0; i < pieces.length; ++i)
-				result[i + 1] = pieces[i];
-			return result;
-
-		} else {
-			if (File.separator.equals("/")) {
-				// *NIX
-				pieces[0] = "/"; // instead of ""
-				return pieces;
-
-			} else if (File.separator.equals("\\")) {
-				// Windows
-				return pieces;
-
-			} else {
-				throw new IllegalStateException(
-						"Unsupported operating system! Please report this.");
-			}
-		}
-	}
-
-	/**
 	 * Return the absolute path corresponding to the given path. We cannot trust
 	 * of File.getAbsolutePath(). Wildchards supported.
 	 */
 	protected String getAbsolutePath(String path) {
 
-		if (isAbsolute(path))
-			return path;
-
-		// here, the path is relative, and it is not null nor empty.
-
-		return getCurrentFolder() + File.separator + path.trim();
+		return shell.getAbsolutePath(path);
 	}
 
-	private void expandRecursive(File root, Stack<String> pieces,
-			Set<String> ret) {
-
-		// DEBUG CODE
-		// System.out.println("DEBUG. entering expandRecursive(" + root + ", " +
-		// pieces + ", " + ret);
-
-		if (!root.exists())
-			return;
-
-		if (pieces.isEmpty() || !root.isDirectory()) {
-			ret.add(root.getPath());
-			return;
-		}
-
-		String nextPiece = pieces.pop();
-		if (!nextPiece.contains("*") && !nextPiece.contains("?")) {
-			// We use a different algorithm in order to see hidden files too
-			File son = new File(root, nextPiece);
-			if (son.exists()) {
-				expandRecursive(son, pieces, ret);
-			}
-		} else {
-
-			final Pattern p = Pattern.compile(nextPiece.replace("*", ".*")
-					.replace("?", ".{1}"));
-			String[] files = root.list(new FilenameFilter() {
-				@Override
-				public boolean accept(File dir, String filename) {
-					return p.matcher(filename).matches();
-				}
-			});
-
-			for (String f : files) {
-				expandRecursive(new File(root, f), pieces, ret);
-			}
-		}
-
-		pieces.push(nextPiece);
-
-		// DEBUG CODE
-		// System.out.println("DEBUG. exiting expandRecursive(" + root + ", " +
-		// pieces + ", " + ret);
-	}
-
-	/**
-	 * Shell-expansion of arguments
-	 */
-	public List<String> expand(List<String> paths) {
-		if (paths == null)
-			throw new IllegalArgumentException("null paths given");
-
-		// DEBUG CODE
-		// System.out.println("expand:" + paths);
-
-		Set<String> ret = new HashSet<String>();
-		for (String path : paths) {
-
-			if (path == null)
-				continue; // should not happen ... but...
-
-			if (!path.contains("*") && !path.contains("?")) {
-				// e.g. a fixed filename, empty string, or options
-				ret.add(path);
-				continue;
-			}
-
-			// here, we *must* perform expansion
-
-			String[] pieces = splitRoot(path);
-
-			// DEBUG CODE
-			//System.err.println("SPLITROOT path=" + path + " pieces="
-			//		+ Arrays.asList(pieces) + " pwd=" + getCurrentFolder());
-
-			File root = new File(pieces[0]);
-
-			Stack<String> piecesStack = new Stack<String>();
-			for (int i = pieces.length - 1; i > 0; --i)
-				piecesStack.push(pieces[i]);
-
-			expandRecursive(root, piecesStack, ret);
-		}
-		return new ArrayList<String>(ret);
-	}
 }
--- src/main/java/javax/shell/Shell.java
+++ src/main/java/javax/shell/Shell.java
@@ -1,7 +1,15 @@
 package javax.shell;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+import java.util.regex.Pattern;
 
 /**
  * The environment where {@see Process}es run. The user is supposed <i>not</i>
@@ -67,4 +75,158 @@ public class Shell {
 	 */
 	private Map<String, String> env = new HashMap<String, String>();
 
+	/**
+	 * Return true if path is absolute (i.e. /some/p*ath/ or C:\so?me\path ).
+	 */
+	protected static boolean isAbsolute(String path) {
+
+		if (path == null)
+			throw new IllegalArgumentException("null path given");
+		path = path.trim();
+		if (path.equals(""))
+			throw new IllegalArgumentException("empty path given");
+
+		if (File.separator.equals("/")) {
+			// *NIX
+			return path.length() >= 1 && path.charAt(0) == '/';
+
+		} else if (File.separator.equals("\\")) {
+			// Windows
+			return path.length() >= 2 && path.charAt(1) == ':';
+
+		} else {
+			throw new IllegalStateException("Unsupported operating system! Please report this.");
+		}
+	}
+
+	/**
+	 * Split the path in pieces according to File.separator. The first piece is
+	 * the "root": it is the path's root ( "/" or "C:\" ) if the given path is
+	 * absolute, or current folder if it is relative.
+	 */
+	protected String[] splitRoot(String path) {
+
+		String[] pieces = path.split(File.separator);
+
+		if (!isAbsolute(path)) {
+
+			String[] result = new String[pieces.length + 1];
+			result[0] = getCurrentFolder();
+			for (int i = 0; i < pieces.length; ++i)
+				result[i + 1] = pieces[i];
+			return result;
+
+		} else {
+			if (File.separator.equals("/")) {
+				// *NIX
+				pieces[0] = "/"; // instead of ""
+				return pieces;
+
+			} else if (File.separator.equals("\\")) {
+				// Windows
+				return pieces;
+
+			} else {
+				throw new IllegalStateException("Unsupported operating system! Please report this.");
+			}
+		}
+	}
+
+	/**
+	 * Return the absolute path corresponding to the given path. We cannot trust
+	 * of File.getAbsolutePath(). Wildchards supported.
+	 */
+	protected String getAbsolutePath(String path) {
+
+		if (isAbsolute(path))
+			return path;
+
+		// here, the path is relative, and it is not null nor empty.
+
+		return getCurrentFolder() + File.separator + path.trim();
+	}
+
+	private void expandRecursive(File root, Stack<String> pieces, Set<String> ret) {
+
+		// DEBUG CODE
+		// System.out.println("DEBUG. entering expandRecursive(" + root + ", " +
+		// pieces + ", " + ret);
+
+		if (!root.exists())
+			return;
+
+		if (pieces.isEmpty() || !root.isDirectory()) {
+			ret.add(root.getPath());
+			return;
+		}
+
+		String nextPiece = pieces.pop();
+		if (!nextPiece.contains("*") && !nextPiece.contains("?")) {
+			// We use a different algorithm in order to see hidden files too
+			File son = new File(root, nextPiece);
+			if (son.exists()) {
+				expandRecursive(son, pieces, ret);
+			}
+		} else {
+
+			final Pattern p = Pattern.compile(nextPiece.replace("*", ".*").replace("?", ".{1}"));
+			String[] files = root.list(new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String filename) {
+					return p.matcher(filename).matches();
+				}
+			});
+
+			for (String f : files) {
+				expandRecursive(new File(root, f), pieces, ret);
+			}
+		}
+
+		pieces.push(nextPiece);
+
+		// DEBUG CODE
+		// System.out.println("DEBUG. exiting expandRecursive(" + root + ", " +
+		// pieces + ", " + ret);
+	}
+
+	/**
+	 * Shell-expansion of arguments
+	 */
+	public List<String> expand(List<String> paths) {
+		if (paths == null)
+			throw new IllegalArgumentException("null paths given");
+
+		// DEBUG CODE
+		// System.out.println("expand:" + paths);
+
+		Set<String> ret = new HashSet<String>();
+		for (String path : paths) {
+
+			if (path == null)
+				continue; // should not happen ... but...
+
+			if (!path.contains("*") && !path.contains("?")) {
+				// e.g. a fixed filename, empty string, or options
+				ret.add(path);
+				continue;
+			}
+
+			// here, we *must* perform expansion
+
+			String[] pieces = splitRoot(path);
+
+			// DEBUG CODE
+			// System.err.println("SPLITROOT path=" + path + " pieces="
+			// + Arrays.asList(pieces) + " pwd=" + getCurrentFolder());
+
+			File root = new File(pieces[0]);
+
+			Stack<String> piecesStack = new Stack<String>();
+			for (int i = pieces.length - 1; i > 0; --i)
+				piecesStack.push(pieces[i]);
+
+			expandRecursive(root, piecesStack, ret);
+		}
+		return new ArrayList<String>(ret);
+	}
 }
